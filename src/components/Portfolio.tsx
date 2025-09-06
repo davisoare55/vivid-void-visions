5cf6543b3a3bcf76bb4e440e8003db2751652084
davisoare55 <100055909+davisoare55@users.noreply.github.com>
2025-09-06 01:11:42 -0300

    Reduce vertical spacing between video player and CTA button: remove mb-4 and reduce mt-6 to mt-2

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -116,7 +116,7 @@ const Portfolio = ({ showFullSite }: PortfolioProps) => {
         {/* Featured Video */}
         <div className="mb-20">
             <div className="relative w-full md:max-w-6xl md:mx-auto px-0 md:px-4">
-              <div className="rounded-lg p-0 md:p-4 lg:p-6 xl:p-8 mb-4">
+              <div className="rounded-lg p-0 md:p-4 lg:p-6 xl:p-8 mb-0">
                 <div 
                   ref={videoContainerRef}
                   className="relative w-full rounded-none md:rounded-lg overflow-hidden bg-background-tertiary border-0 md:border border-border/50 video-container"
@@ -131,7 +131,7 @@ const Portfolio = ({ showFullSite }: PortfolioProps) => {
                 />
               </div>
               {showFullSite && (
-                <div className="flex justify-center mt-6">
+                <div className="flex justify-center mt-2">
                   <a 
                     href="https://docs.google.com/forms/d/e/1FAIpQLSfadJIhA1H410Cj_6Mxs8kEj6bupPDbivUqiWZPR0_pqt7wlQ/viewform?usp=header"
                     target="_blank"
